675bda091741785ce7d597da4ba4362b602145a8
yushibatan <y-shibata@tokyo-gas.co.jp>
2024-07-14 11:59:45 +0900

    課題５をアップロード

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+// 変数の宣言
+let bmiHeight;
+let bmiWeight;
+let bmi;
+
+// 値の代入
+bmiHeight = 1.7;
+bmiWeight = 68;
+bmi = bmiWeight / (bmiHeight * bmiHeight);
+
+//BMIの計算、コンソールへの出力
+console.log(bmi);
