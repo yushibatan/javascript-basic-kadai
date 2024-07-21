75c5f6b12a33e6164555986009e702878ec6e021
yushibatan <y-shibata@tokyo-gas.co.jp>
2024-07-21 14:52:11 +0900

    課題２０をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+
+    // id="text"の文章を書き換えるには、textContent を使う。
+    text.textContent    = "ボタンをクリックしました。";    
+});
